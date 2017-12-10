08f6b5037c90ae0eeaec39bc5afc87cd90422a2b
Maryna Hapon <marynahapon@gmail.com>
2017-12-10 13:53:40 +0200

    root app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AbonentsComponent } from './abonents/abonents.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AbonentsComponent
   ],
   imports: [
     BrowserModule
