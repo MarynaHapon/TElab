0df1bd70659abb507b7b7d2ea9f28ae89415bf1d
Maryna Hapon <marynahapon@gmail.com>
2017-12-10 13:54:23 +0200

    abonents

---
+++ src/app/abonents/abonents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-abonents',
+  templateUrl: './abonents.component.html',
+  styleUrls: ['./abonents.component.css']
+})
+export class AbonentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
