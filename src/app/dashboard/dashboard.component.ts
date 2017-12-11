b405e450bb7aca4d07b5427312dfcde4b43c62e4
Maryna Hapon <marynahapon@gmail.com>
2017-12-11 14:12:06 +0200

    dashboard / ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Abonent } from '../abonent';
+import { AbonentService } from '../abonent.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
+  abonents: Abonent[] = [];
 
-  constructor() { }
+  constructor(private abonentService: AbonentService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.abonentService.getAbonents()
+      .subscribe(abonents => this.abonents = abonents.slice(1, 5));
+  }
 }
